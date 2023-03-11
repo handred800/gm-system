833a23e6b81dd6a3339a383d3dcd8e7dda91aabb
handred800 <handred800@gmail.com>
2023-03-11 01:25:46 +0000

    更新 AI

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import { useEffect, useState } from 'react';
+import { useAI } from './hooks/useAIChoice';
 import * as GM from './gm';
 
 export default function App() {
   const [player, setPlayer] = useState(null);
   const [opposition, setOpposition] = useState(null);
   const [turnCount, setTurnCount] = useState(0);
+  const [isPlayerTurn, setPlayerTurn] = useState(true);
+  const [makeChoice, currentChoice] = useAI(['attack']);
 
   useEffect(() => {
     setPlayer(GM.playerInit({ name: 'Vivan' }));
@@ -12,11 +15,16 @@ export default function App() {
   }, []);
 
   useEffect(() => {
-    console.log(`now turn: ${turnCount}`);
+    console.log(`current turn: ${turnCount}, ${isPlayerTurn ? 'player' : 'opposition'} turn`);
+    if (!isPlayerTurn) {
+      makeChoice();
+      console.log(`opposition choose ${currentChoice}`);
+    }
   }, [turnCount]);
 
-  function attack({sender, receiver}) {
+  function attack({ sender, receiver }) {
     setTurnCount((prev) => (prev += 1));
+    setPlayerTurn(!isPlayerTurn);
     console.log(`${sender} attack ${receiver}`);
   }
 
